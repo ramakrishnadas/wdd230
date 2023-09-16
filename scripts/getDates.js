3e9f784e75f3580408b17d01bfcbc3399f722a93
ramakrishnadas <rama.k.d.b.p@gmail.com>
2023-09-16 16:31:14 -0400

    completed course home page development activity

---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+// dynamically update current year for copyright in footer section
+let currentYear = new Date();
+document.querySelector('#currentYear').textContent = currentYear.getFullYear();
+
+// last modified date in footer section
+let lastModifiedDate = document.lastModified;
+document.querySelector('#lastModified').textContent = `Last Modification: ${lastModifiedDate}`;
+
