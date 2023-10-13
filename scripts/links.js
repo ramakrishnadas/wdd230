1aa6bb7828fb40edebef19f59b2aa84a7f16d9a6
ramakrishnadas <rama.k.d.b.p@gmail.com>
2023-10-13 01:37:37 -0400

    step 2 home page activity

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,18 @@
 const baseURL = 'https://ramakrishnadas.github.io/wdd230/';
-const linksURL = 'https://ramakrishnadas.github.io/wdd230/data/links.json'
+const linksURL = 'https://ramakrishnadas.github.io/wdd230/data/links.json';
+
+async function getLinks(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            //displayLinks(data);
+        } else {
+            throw Error(await response.text());
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
