ba201c1102b29ce2719a23ebff16e58f45361fc2
ramakrishnadas <rama.k.d.b.p@gmail.com>
2023-10-13 01:30:31 -0400

    first step for home page activity

---
+++ scripts/links.js
@@ -0,0 +1,2 @@
+const baseURL = 'https://ramakrishnadas.github.io/wdd230/';
+const linksURL = 'https://ramakrishnadas.github.io/wdd230/data/links.json'
---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=10.48155789576067&lon=-66.91096611098436&units=imperial&appid=d13b07c3eea7b641ed4c2925c810a8a9';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch(url);
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main["temp"]}`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+
+}
